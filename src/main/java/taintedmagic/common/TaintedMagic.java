634d8e480e318a457d7af57751d8c10fc860436a
Maya <10861407+serenibyss@users.noreply.github.com>
2025-04-23 15:01:34 -0500

    update

--- src/main/java/taintedmagic/common/TaintedMagic.java
+++ src/main/java/taintedmagic/common/TaintedMagic.java
@@ -47,7 +47,7 @@ public class TaintedMagic {
 
     public static final String MOD_ID = "TaintedMagic";
     public static final String MOD_NAME = "Tainted Magic";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String VERSION = TMTags.VERSION;
     public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,);required-after:Thaumcraft@[4.2.3.5,)";
 
     public static TaintedMagicEventHandler taintedMagicEvents;
